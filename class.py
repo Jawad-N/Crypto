6eb2e4bef34df54662a823701d05c98c8f7296bb
jawad <jmn09@mail.aub.edu>
2023-10-26 15:03:02 +0300

    sla2a

--- class.py
+++ class.py
@@ -1,7 +1,17 @@
 import math
-
+m = 163
 class polynomial(object):
-    def __init__(self):
-        self.x = 1
+    def __init__(self, L):
+        self.m = len(L)
+        self.pol = L
 
+    def add(self, other):
+        assert(self.m == len(other)), "error"
+
+    def subtract(self,other):
+
+    def multiplication(self,other):
+
+    def inverse(self, other):
+        
     
