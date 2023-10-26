0e890b4b920f08103ce55c8ed1f6df93b5bdaf17
jawad <jmn09@mail.aub.edu>
2023-10-26 16:22:23 +0300

    mul, shifts

--- class.py
+++ class.py
@@ -1,8 +1,34 @@
 import math
 
-def irreducible(m): # given GF(2^m) we want to return the irreducible polynomial of this field
+#def irreducible(m): # given GF(2^m) we want to return the irreducible polynomial of this field
                     # we can't do it by bruteforce, for big m it is computationally impossible
     
+
+def leftShift(L):# careful also affects L, this multiplies the polynomial by x
+    n = len(L)
+    L.append(0)
+    for i in range(n, 0, -1):
+        L[i] = L[i-1]
+    L[0] = 0
+    return L
+
+def rightShift(L):# this divides the polynomial by x, if 1 exists in the polynomial, then it substract it before doing so
+    n = len(L)
+    for i in range(1,n):
+        L[i-1] = L[i]
+    L.pop()
+    return L
+
+def mul(p1,p2):
+    assert( type(p1) == polynomial and type(p2) == polynomial ), "multiplying non polinomial objects"
+    res = [0] * (p1.m + p2.m - 1)
+    for i in range(p1.m):
+        for j in range(p2.m):
+            res[i+j] += p1.pol[i] * p2.pol[j]
+    for i in range(len(res)):
+        res[i] %= 2
+    return polynomial(res)
+
 
 
 class polynomial(object):
@@ -10,7 +36,12 @@ class polynomial(object):
         self.m = len(L)
         self.pol = L
         for i in range(self.m):
-            self.poly[i] %= 2
+            self.pol[i] %= 2
+
+    def modulo(self):
+        irred = irred(self.m)
+        len(self.poly)
+
         
 
     def add(self, other):
@@ -36,6 +67,9 @@ class polynomial(object):
                 self.pol[i] = self.pol[i] - other.pol[i]
 
 
+    
+
+
     def multiplication(self,other):
         assert(self.m == other.m), "error"
 
@@ -53,11 +87,16 @@ class polynomial(object):
         else: return " + ".join(ans)
 
 
-L = [1,2,3,4,5,6]
+L = [0,0,0,0,1,1]
 L2 = [0,0,0,0,1,1]
 poly1 = polynomial(L)
 poly2 = polynomial(L2)
-print(poly1.add(poly2))
+print(poly1)
+print(poly2)
+poly3 = mul(poly1, poly2)
+print(poly3)
+print(leftShift(L))
+print(rightShift(L))
 
 
     
