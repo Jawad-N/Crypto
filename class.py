f32a983450d76a111b942fe84f7d9132e1d0130c
alexjuv <agj06@mail.aub.edu>
2023-10-26 15:20:45 +0300

    Alex commit

--- class.py
+++ class.py
@@ -6,9 +6,17 @@ class polynomial(object):
         self.pol = L
 
     def add(self, other):
-        assert(self.m == len(other)), "error"
+        assert(self.m == other.m), "error"
 
     def subtract(self,other):
+        assert(self.m == other.m), "error"
+        #We are subtracting the second polynomial from the first polynomial
+        for i in range(self.m):
+            if(self.pol[i] < other.pol[i]):
+                self.pol[i] = 1
+            else:
+                self.pol[i] = self.pol[i] - other.pol[i]
+        
 
     def multiplication(self,other):
 
