91327e0b6a79dc2204f577653f6dd1a1b844604a
jawad <jmn09@mail.aub.edu>
2023-11-10 08:08:56 +0200

    hi

--- class.py
+++ class.py
@@ -1,41 +1,13 @@
 import math
 from copy import deepcopy
-# Using Optimal irreducible polynomials for Pentium-type 32-bit processor
+# Using Optimal irreducible polynomials for Pentium-type 32-bit processor - List in the sent article
 
 
-
-#def irreducible(m): # given GF(2^m) we want to return the irreducible polynomial of this field
-                    # we can't do it by bruteforce, for big m it is computationally impossible
-
-#we always consider the irreducible polynomial that is m(x) = x^m + x + 1
-#we know it is irreducible because m(0) = 1 and m(1) = 1 thus it has no roots --> it cannot be factorized
- 
-def leftShift(L):# careful also affects L, this multiplies the polynomial by x
-    n = len(L)
-    L.append(0)
-    for i in range(n, 0, -1):
-        L[i] = L[i-1]
-    L[0] = 0
-    return L
-
-def rightShift(L):# this divides the polynomial by x, if 1 exists in the polynomial, then it substract it before doing so
-    n = len(L)
-    for i in range(1,n):
-        L[i-1] = L[i]
-    L.pop()
-    return L
-
-def mul(p1,p2):
-    assert( type(p1) == polynomial and type(p2) == polynomial ), "multiplying non polinomial objects"
-    res = [0] * (p1.m + p2.m + 1)
-    for i in range(len(p1.pol)):
-        for j in range(len(p2.pol)):
-            res[i+j] += p1.pol[i] * p2.pol[j] # can implemented in O(nlogn using FFT) 
-    for i in range(len(res)):
-        res[i] %= 2
-    return polynomial(res)
 
 class polynomial(object):
+
+    
+
 
     def __init__(self, L): 
         self.m = len(L)-1
@@ -43,7 +15,6 @@ class polynomial(object):
         for i in range(len(L)):
             self.pol[i] %= 2
       
-
     def add(self, other):
         assert(self.m == other.m), "error"
         res = []
@@ -53,7 +24,6 @@ class polynomial(object):
             res[-1] %= 2
         return polynomial(res)
     
-
     def subtract(self,other):
         #We are subtracting the second polynomial from the first polynomial
         res = [0] * (self.m + 1)
@@ -64,6 +34,15 @@ class polynomial(object):
                 res[i] = self.pol[i] - other.pol[i]
         return polynomial(res)
     
+    def __mul(p1,p2):
+        assert( type(p1) == polynomial and type(p2) == polynomial ), "multiplying non polinomial objects"
+        res = [0] * (p1.m + p2.m + 1)
+        for i in range(len(p1.pol)):
+            for j in range(len(p2.pol)):
+                res[i+j] += p1.pol[i] * p2.pol[j] # can implemented in O(nlogn using FFT) 
+        for i in range(len(res)):
+            res[i] %= 2
+        return polynomial(res)
 
     def division(self,other):
         current = polynomial( self.pol )
@@ -74,21 +53,16 @@ class polynomial(object):
             ans.pol[ current.degree() - other.degree() ] = 1
             temp = polynomial( [0] * ( current.degree()-other.degree()+1 ) )
             temp.pol[-1] = 1
-            current = current.subtract( mul( other, temp ) ) # here we use the first multiplication that is not modular cause we need division to stay in the field
-
+            current = current.subtract( other.__mul( temp ) ) # here we use the first multiplication that is not modular cause we need division to stay in the field
         rem = current
         return ans, rem
     
-
-
-
     def multiplication(self,other):
-        curr = mul(self, other)
+        curr = self.__mul( other )
         return curr.division(D[self.m])[1] 
     # returning the remainder of the division between the polynomial of 
     # the regular multiplication ignoring the field limits, which is then
     # moduloed with the field irreducible polynomial
-
 
     def degree(self):
         for i in range(len(self.pol)-1,-1,-1):
@@ -96,11 +70,7 @@ class polynomial(object):
                 return i
         return -1
 
-
- 
-
     def modularInverse(self):
-
         mod = D[self.m]
         print("here: ",self.m)
         zero = [0] * ( self.m+1 )
@@ -114,20 +84,14 @@ class polynomial(object):
         one = polynomial(one)
         while( not (B[2] == one) ):
             (Q, R) = A[2].division(B[2])
-            print("Qutoient & remainder: ", Q, R)
-
             C = [0, 0, 0]
             C[0] = A[0].subtract( Q.multiplication(B[0]) )
             C[1] = A[1].subtract( Q.multiplication(B[1]) )
             C[2] = R
-            for j in range(3): print(C[j].m)
-            print('\n')
             A = B
             B = C
-
         return B[1]
     
-
     def __eq__(self, other):
         if( self.m != other.m ): return False
         else:
@@ -136,7 +100,6 @@ class polynomial(object):
                     return False
             return True
        
-
     def __str__(self):
         cond = False
         ans = []
@@ -146,7 +109,6 @@ class polynomial(object):
                 ans.append(f"x^{i}")
         if(len(ans) == 0): return "0"
         else: return " + ".join(ans)
-
 
 m3 = [0] * 4
 m8 = [0] * 9
@@ -195,7 +157,6 @@ D = {
     283: m283 ,
     409: m409 ,
     571: m571
-
 }
 
 
